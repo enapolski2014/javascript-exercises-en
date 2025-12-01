1e25f78ece1d29e2f401e90d7c0b49274726a656
enapolski2014 <enapolski2014@gmail.com>
2025-11-30 19:29:05 -0600

    solved tempCon...js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
-};
+// from f to c: c = (f-32) / 1.8
 
-const convertToFahrenheit = function() {
-};
+const convertToCelsius = function(fT) {
+  let long = (fT - 32)/1.8;
+  let rounded = Math.round(long * 10)/10;
+  return rounded;
+  
+  };
+  
+  // from c to f: f = c * 1.8 + 32
+  
+  const convertToFahrenheit = function(cT) {
+      let long = cT * 1.8 + 32;
+      let rounded = Math.round(long * 10)/10;
+      return rounded;
+  };
 
 // Do not edit below this line
 module.exports = {
