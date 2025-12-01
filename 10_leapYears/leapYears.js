98f303400ec42b7131cc01d9de0f451950475842
enapolski2014 <enapolski2014@gmail.com>
2025-11-30 18:35:06 -0600

    solved, probably not efficiently

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (Number.isInteger(year/100) && !Number.isInteger(year/400)) {
+        // alert('1');
+        return false;
+    } else if (Number.isInteger(year/100) && Number.isInteger(year/400)) {
+        // alert('2');
+        return true;
+    } else if (!Number.isInteger(year/4)) {
+        // alert ('3')
+        return false;
+    } else if (Number.isInteger(year/4)) {
+        // alert('4');
+        return true;
+    }
 
 };
+
+
+console.log(leapYears(1996))
 
 // Do not edit below this line
 module.exports = leapYears;
